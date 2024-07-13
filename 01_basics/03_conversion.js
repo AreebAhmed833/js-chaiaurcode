772f20c2140e7d651aa88c777a6764db522b0189
Areeb Ahmed <ahmedareebkhalil@gmail.com>
2024-07-13 14:45:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+/*let score = "33abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+*/
+// "33" => 33
+// "33abc" => NaN
+// 1 => true
+// 0 => false
+// null => 0
+// undefined => NaN
+
+//let isLoggedIn= "aaif"
+//let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true 
+// 0=> false
+// "" => false
+// "areeb" => true
+
+let someNumber = 33
+console.log(typeof someNumber) // number
+let stringNumber = String(someNumber)
+console.log(stringNumber) //33 
+console.log(typeof stringNumber) // string
+
